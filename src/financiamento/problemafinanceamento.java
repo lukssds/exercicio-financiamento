4eedff0f21c603ce61a557d6a7480f51ca281397
lukssds <lukssds@gmail.com>
2020-05-22 20:28:40 -0300

    Leitura do nome e salario

--- src/financiamento/problemafinanceamento.java
+++ src/financiamento/problemafinanceamento.java
@@ -1,9 +1,31 @@
 package financiamento;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class problemafinanceamento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc= new Scanner (System.in);
+		
+		double salario, totalfinanciado;
+		int prestacoes,opcoes;
+		String nome;
+		
+		System.out.println("DIGITE OS DADOS DO FINANCIAMENTO");
+		System.out.print("Nome do cliente: ");
+		nome= sc.nextLine();
+		System.out.print("Salario: ");
+		salario=sc.nextDouble();
+		
+		while (salario < 0.0) { 
+			System.out.println("Salario não pode ser negativo tente novamante");
+			System.out.print("Salario: ");
+			salario=sc.nextDouble();
+		}
+		
+		sc.close();
 
 	}
 
